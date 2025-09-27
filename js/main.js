c13e7fe37b95d571046f834ebe558aee96a83370
leander@kafemann.berlin <leander@kafemann.berlin>
2025-09-27 11:56:46 +0200

    debug

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         let linkUrl = link.href.split('#')[0].replace(/\/$/, '').replace("index.html", "");
         let pageUrl = currentUrl.replace(/\/$/, '').replace("index.html", "");
 
+        //debug
+        console.log('Link URL:', linkUrl);
+        console.log('Page URL:', pageUrl);
+
         if (linkUrl === pageUrl) {
             link.classList.add('active');
         }
